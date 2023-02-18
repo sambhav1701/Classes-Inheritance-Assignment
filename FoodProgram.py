62a2c19de864b4a5ee00f4b56f75999f7d142a88
Sambhav Jain <sambhav.1701@outlook.com>
2023-02-18 16:58:16 -0600

    run twice left

--- FoodProgram.py
+++ FoodProgram.py
@@ -7,7 +7,7 @@ dict = {'trans1':['2/15/2023','The Lone Patty',17,569],
                 'trans3':['2/15/2023','The Octoveg',16,570],
                 'trans4':['2/15/2023','The Octoburger',20,570],
 }
-order_total = 0
+
 
 customer1 = fc.Customer("570", "Danni Sellyar", "97 Mitchell Way Hewitt Texas 76712", "dsellyarft@gmpg.org", "254-555-9362", False)
 customer2 = fc.Customer("569", "Aubree Himsworth", "1172 Moulton Hill Waco Texas 76710", "ahimsworthfs@list-manage.com", "254-555-2273", True)
@@ -18,36 +18,28 @@ customer2 = fc.Customer("569", "Aubree Himsworth", "1172 Moulton Hill Waco Texas
 #transaction3 = fc.Transaction(dict['trans3'][0], dict['trans3'][1], dict['trans3'][2], dict['trans3'][3])
 #transaction4 = fc.Transaction(dict['trans4'][0], dict['trans4'][1], dict['trans4'][2], dict['trans4'][3])
 
-
-print("Customer Name: ",customer1.get_name())
-print("Phone: ",customer1.get_phone())
-cost = 0
+transaction_instance = []
 for key, value in dict.items():
-    if str(value[3]) == customer1.get_customerid():
-        if customer1.is_member:
-            discountprice = value[2] * 0.8
-            print("Order Item:" , value[1] , "Price =" , value[2])
-        else:
-            print("Order Item:" , value[1] , "Price =" , value[2])
-        cost += value[2] 
-print("Total Cost:", cost)
+    transaction = fc.Transaction(value[0], value[1], value[2], value[3])
+    transaction_instance.append(transaction)
+    #print(transaction_instance)
 
+for customer in [customer1, customer2]:
+    order_total = 0
 
-print("Customer Name: ",customer2.get_name())
-print("Phone: ",customer2.get_phone())
-cost = 0
-for key, value in dict.items():
-    if str(value[3]) == customer2.get_customerid():
-        if customer2.is_member:
-            discountprice = value[2] * 0.8
-            print("Order Item:" , value[1] , "Price =" , value[2])
-        else:
-            print("Order Item:" , value[1] , "Price =" , value[2])
-        cost += value[2] 
-print("Total Cost:", cost)
-print("Member Discount: ", cost * 0.8)
-print("Total Cost after discount:", cost - (cost * 0.8))
-                   
-        
+    print("Customer Name: ",customer.get_name())
+    print("Phone: ",customer.get_phone())
 
+    for transaction in transaction_instance:
+        if int(transaction.get_customerid()) == int(customer.get_customerid()):
+            order_total += transaction.get_cost()
+            print("Order Item:" , transaction.get_itemname() , "Price =" , transaction.get_cost())
+            
 
+    if customer.is_member():
+        discount = order_total * 0.2
+        print("Total Cost:", order_total)
+        print("Member Discount: ", discount)
+        print("Total Cost after discount:", order_total - discount)
+    else:
+        print("Total Cost:", order_total)
